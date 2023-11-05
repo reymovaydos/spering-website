bc7725ddf84d3db05a56c0d59795011fa53d9d75
reymov aydos <reymovaydos83@gmail.com>
2023-11-05 23:10:20 +0500

    state changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import { useState } from 'react'
 import './App.css'
 import { CounterComp } from './components/counter/counter'
 
 function App() {
-
+  const [count, setCount ] = useState(0)
   return (
     <div className='App'>
       <CounterComp/>
