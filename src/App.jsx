0484d8c60ffc5b847d24d320ccc83ac60d5dccc8
reymov aydos <reymovaydos83@gmail.com>
2023-11-05 23:08:46 +0500

    done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { CounterComp } from './components/counter/counter'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <CounterComp/>
+    </div>
+  )
+}
+
+export default App
